c73d00378cc0ebce923814baa9fe29cf4b157134
Elie <niyubwayoiraelie5777@gmail.com>
2026-01-09 19:02:19 +0200

    Implemented ProductService & Imported product

--- src/main/java/dev/iraelie/e_commerce_backend/repository/ProductRepository.java
+++ src/main/java/dev/iraelie/e_commerce_backend/repository/ProductRepository.java
@@ -1,5 +1,6 @@
 package dev.iraelie.e_commerce_backend.repository;
 
+import dev.iraelie.e_commerce_backend.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public class ProductRepository implements JpaRepository<Product, Long> {
--- src/main/java/dev/iraelie/e_commerce_backend/service/product/ProductService.java
+++ src/main/java/dev/iraelie/e_commerce_backend/service/product/ProductService.java
@@ -1,4 +1,62 @@
 package dev.iraelie.e_commerce_backend.service.product;
 
-public class ProductService {
+import dev.iraelie.e_commerce_backend.model.Product;
+
+import java.util.List;
+
+public class ProductService implements IProductService{
+    @Override
+    public Product addProduct(Product product) {
+        return null;
+    }
+
+    @Override
+    public void deleteProductById(Long id) {
+
+    }
+
+    @Override
+    public Product updateProduct(Product product, Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return List.of();
+    }
+
+    @Override
+    public Product getProductById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Product> getProductsByName(String name) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByBrand(String brand) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByCategoryName(String category) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByCategoryAndBrand(String category, String brand) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByBrandAndName(String category, String name) {
+        return List.of();
+    }
+
+    @Override
+    public Long countProductsByBrandAndName(String brand, String name) {
+        return 0L;
+    }
 }
