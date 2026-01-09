22b38bb5b1e4f93096a2f42e9cdf3e0ff4ca29a5
Elie <niyubwayoiraelie5777@gmail.com>
2026-01-09 18:55:22 +0200

    Implemented ProductRepository and Interface

---
+++ src/main/java/dev/iraelie/e_commerce_backend/repository/ProductRepository.java
@@ -0,0 +1,6 @@
+package dev.iraelie.e_commerce_backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class ProductRepository implements JpaRepository<Product, Long> {
+}
--- src/main/java/dev/iraelie/e_commerce_backend/service/product/IProductService.java
+++ src/main/java/dev/iraelie/e_commerce_backend/service/product/IProductService.java
@@ -1,4 +1,19 @@
 package dev.iraelie.e_commerce_backend.service.product;
 
+import dev.iraelie.e_commerce_backend.model.Product;
+
+import java.util.List;
+
 public interface IProductService {
+    Product addProduct(Product product);
+    void deleteProductById(Long id);
+    Product updateProduct(Product product, Long id);
+    List<Product> getAllProducts();
+    Product getProductById(Long id);
+    List<Product> getProductsByName(String name);
+    List<Product> getProductsByBrand(String brand);
+    List<Product> getProductsByCategoryName(String category);
+    List<Product> getProductsByCategoryAndBrand(String category, String brand);
+    List<Product> getProductsByBrandAndName(String category, String name);
+    Long countProductsByBrandAndName(String brand, String name);
 }
