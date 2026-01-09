bcadf0a07cf6d3da5042081cbbd3482b03171b45
Elie <niyubwayoiraelie5777@gmail.com>
2026-01-09 20:21:28 +0200

    Turned it into an interface

--- src/main/java/dev/iraelie/e_commerce_backend/repository/ProductRepository.java
+++ src/main/java/dev/iraelie/e_commerce_backend/repository/ProductRepository.java
@@ -5,16 +5,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public class ProductRepository implements JpaRepository<Product, Long> {
-    public List<Product> findByName(String name) {}
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByName(String name);
 
-    public List<Product> findByBrand(String brand) {}
+    List<Product> findByBrand(String brand);
 
-    public List<Product> findByCategory(String category) {}
+    List<Product> findByCategory(String category);
 
-    public List<Product> findByCategoryAndBrand(String category, String brand) {}
+    List<Product> findByCategoryAndBrand(String category, String brand);
 
-    public List<Product> findByBrandAndName(String category, String name) {}
+    List<Product> findByBrandAndName(String category, String name);
 
-    public Long countByBrandAndName(String brand, String name) {}
+    Long countByBrandAndName(String brand, String name);
 }
