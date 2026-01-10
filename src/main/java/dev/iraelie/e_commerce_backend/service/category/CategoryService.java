1c47251f67c253ca1a571d67effa306d25b90db9
Elie <niyubwayoiraelie5777@gmail.com>
2026-01-10 23:16:43 +0200

    Implemented methods in CategoryService.

--- src/main/java/dev/iraelie/e_commerce_backend/service/category/CategoryService.java
+++ src/main/java/dev/iraelie/e_commerce_backend/service/category/CategoryService.java
@@ -1,4 +1,37 @@
 package dev.iraelie.e_commerce_backend.service.category;
 
-public class CategoryService {
+import dev.iraelie.e_commerce_backend.model.Category;
+
+import java.util.List;
+
+public class CategoryService implements ICategoryService{
+    @Override
+    public Category getCategoryById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Category getCategoryByName(String name) {
+        return null;
+    }
+
+    @Override
+    public List<Category> getAllCategories() {
+        return List.of();
+    }
+
+    @Override
+    public Category addCategory(Category category) {
+        return null;
+    }
+
+    @Override
+    public Category updateCategory(Category category) {
+        return null;
+    }
+
+    @Override
+    public void deleteCategoryById(Long id) {
+
+    }
 }
