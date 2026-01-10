68faac99f932f40ac035f676b0fcff103300f676
Elie <niyubwayoiraelie5777@gmail.com>
2026-01-10 23:15:19 +0200

    Implemented Category interface

--- src/main/java/dev/iraelie/e_commerce_backend/service/category/ICategoryService.java
+++ src/main/java/dev/iraelie/e_commerce_backend/service/category/ICategoryService.java
@@ -1,4 +1,14 @@
 package dev.iraelie.e_commerce_backend.service.category;
 
+import dev.iraelie.e_commerce_backend.model.Category;
+
+import java.util.List;
+
 public interface ICategoryService {
+    Category getCategoryById(Long id);
+    Category getCategoryByName(String name);
+    List<Category> getAllCategories();
+    Category addCategory(Category category);
+    Category updateCategory(Category category);
+    void deleteCategoryById(Long id);
 }
