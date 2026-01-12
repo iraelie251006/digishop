ec8c5fd446f63e340215c52edf42a0f0aa89e339
Elie <niyubwayoiraelie5777@gmail.com>
2026-01-12 23:11:41 +0200

    Implemented update Image functionality.

--- src/main/java/dev/iraelie/e_commerce_backend/service/image/ImageService.java
+++ src/main/java/dev/iraelie/e_commerce_backend/service/image/ImageService.java
@@ -8,6 +8,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.sql.rowset.serial.SerialBlob;
+import java.io.IOException;
+import java.sql.SQLException;
+
 @Service
 @RequiredArgsConstructor
 public class ImageService implements IImageService{
@@ -41,6 +45,14 @@ public class ImageService implements IImageService{
 
     @Override
     public void updateImage(MultipartFile file, Long imageId) {
-
+        Image image = getImageById(imageId);
+        try {
+            image.setFileName(file.getOriginalFilename());
+            image.setFileType(file.getContentType());
+            image.setImage(new SerialBlob(file.getBytes()));
+            imageRepository.save(image);
+        } catch (IOException | SQLException e) {
+            throw new RuntimeException(e.getMessage());
+        }
     }
 }
