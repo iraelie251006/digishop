b12d4ca06cdbb121a2bce45919471d5d7b5f9629
Elie <niyubwayoiraelie5777@gmail.com>
2026-01-12 23:15:30 +0200

    Implemented ImageDto fields.

--- src/main/java/dev/iraelie/e_commerce_backend/dto/ImageDto.java
+++ src/main/java/dev/iraelie/e_commerce_backend/dto/ImageDto.java
@@ -1,4 +1,10 @@
 package dev.iraelie.e_commerce_backend.dto;
 
+import lombok.Data;
+
+@Data
 public class ImageDto {
+    private Long id;
+    private String fileName;
+    private String downloadUrl;
 }
