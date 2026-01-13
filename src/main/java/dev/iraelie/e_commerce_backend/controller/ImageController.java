4d1250d6009510e10b7ce6f2f2b1527a31b948d9
Elie <niyubwayoiraelie5777@gmail.com>
2026-01-13 22:52:04 +0200

    Initialized ImageController & injected ImageService dependency.

--- src/main/java/dev/iraelie/e_commerce_backend/controller/ImageController.java
+++ src/main/java/dev/iraelie/e_commerce_backend/controller/ImageController.java
@@ -1,4 +1,13 @@
 package dev.iraelie.e_commerce_backend.controller;
 
+import dev.iraelie.e_commerce_backend.service.image.IImageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("${api.prefix}/images")
 public class ImageController {
+    private final IImageService imageService;
 }
