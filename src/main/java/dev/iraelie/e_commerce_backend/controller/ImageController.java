9e5c96f8d775b0ef940a0bd09cb7a4a2f7851f3d
Elie <niyubwayoiraelie5777@gmail.com>
2026-01-13 23:01:00 +0200

    Refactored saveImages params order.

--- src/main/java/dev/iraelie/e_commerce_backend/controller/ImageController.java
+++ src/main/java/dev/iraelie/e_commerce_backend/controller/ImageController.java
@@ -24,7 +24,7 @@ public class ImageController {
     @PostMapping("/upload")
     public ResponseEntity<ApiResponse> saveImages(@RequestParam List<MultipartFile> files, @RequestParam Long productId) {
         try {
-            List<ImageDto> imageDtos = imageService.saveImages(productId, files);
+            List<ImageDto> imageDtos = imageService.saveImages(files, productId);
             return ResponseEntity.ok(new ApiResponse("Upload success!", imageDtos));
         } catch (Exception e) {
             return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("Upload failed!", e.getMessage()));
