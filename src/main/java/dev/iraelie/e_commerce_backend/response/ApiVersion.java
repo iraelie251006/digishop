647c1423d89f29687b21c53cf25bd44481795054
Elie <niyubwayoiraelie5777@gmail.com>
2026-01-13 22:42:15 +0200

    Implemented ApiVersion class to manage responses to the frontend.

--- src/main/java/dev/iraelie/e_commerce_backend/response/ApiVersion.java
+++ src/main/java/dev/iraelie/e_commerce_backend/response/ApiVersion.java
@@ -1,4 +1,11 @@
 package dev.iraelie.e_commerce_backend.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class ApiVersion {
+    private String message;
+    private Object data;
 }
