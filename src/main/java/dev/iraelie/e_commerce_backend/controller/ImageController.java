6a811b93cd4472acde8c0a9ce4ef493c93c140dc
Elie <niyubwayoiraelie5777@gmail.com>
2026-01-13 22:56:23 +0200

    Implemented /upload endpoint.

--- src/main/java/dev/iraelie/e_commerce_backend/controller/ImageController.java
+++ src/main/java/dev/iraelie/e_commerce_backend/controller/ImageController.java
@@ -1,13 +1,33 @@
 package dev.iraelie.e_commerce_backend.controller;
 
+import dev.iraelie.e_commerce_backend.dto.ImageDto;
 import dev.iraelie.e_commerce_backend.service.image.IImageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("${api.prefix}/images")
 public class ImageController {
     private final IImageService imageService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<ApiResponse> saveImages(@RequestParam List<MultipartFile> files, @RequestParam Long productId) {
+        try {
+            List<ImageDto> imageDtos = imageService.saveImages(productId, files);
+            return ResponseEntity.ok(new ApiResponse("Upload success!", imageDtos));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("Upload failed!", e.getMessage()));
+        }
+
+    }
 }
