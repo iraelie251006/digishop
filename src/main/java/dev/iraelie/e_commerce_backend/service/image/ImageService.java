9e1c377273c9c07a7bebe806c3913c80f3c50b42
Elie <niyubwayoiraelie5777@gmail.com>
2026-01-13 22:59:08 +0200

    Refactored ImageService method saveImage to saveImages.

--- src/main/java/dev/iraelie/e_commerce_backend/service/image/ImageService.java
+++ src/main/java/dev/iraelie/e_commerce_backend/service/image/ImageService.java
@@ -43,7 +43,7 @@ public class ImageService implements IImageService{
     }
 
     @Override
-    public List<ImageDto> saveImage(List<MultipartFile> files, Long productId) {
+    public List<ImageDto> saveImages(List<MultipartFile> files, Long productId) {
         Product product = productService.getProductById(productId);
 
         List<ImageDto> savedImageDto = new ArrayList<>();
