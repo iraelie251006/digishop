fbd56b893b420aa39a79dbaf852c9bd91309f3d4
Elie <niyubwayoiraelie5777@gmail.com>
2026-01-14 21:27:06 +0200

    Implemented downloadImage functionality

--- src/main/java/dev/iraelie/e_commerce_backend/controller/ImageController.java
+++ src/main/java/dev/iraelie/e_commerce_backend/controller/ImageController.java
@@ -31,4 +31,12 @@ public class ImageController {
         }
 
     }
+
+    public ResponseEntity<Resource> downloadImage(@PathVariable Long imageId) throws SQLException {
+        Image image = imageService.getImageById(imageId);
+        ByteArrayResource resource = new ByteArrayResource(image.getImage().getBytes(1, (int) image.getImage().length()));
+        return  ResponseEntity.ok().contentType(MediaType.parseMediaType(image.getFileType()))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" +image.getFileName() + "\"")
+                .body(resource);
+    }
 }
