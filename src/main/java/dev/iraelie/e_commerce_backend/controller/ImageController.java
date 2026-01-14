788cce5a77c87d45018f1cb34af142c74ed52730
Elie <niyubwayoiraelie5777@gmail.com>
2026-01-14 21:31:30 +0200

    Added GET endpoint of /image/download/{imageId}

--- src/main/java/dev/iraelie/e_commerce_backend/controller/ImageController.java
+++ src/main/java/dev/iraelie/e_commerce_backend/controller/ImageController.java
@@ -35,6 +35,7 @@ public class ImageController {
 
     }
 
+    @GetMapping("/image/download/{imageId}")
     public ResponseEntity<Resource> downloadImage(@PathVariable Long imageId) throws SQLException {
         Image image = imageService.getImageById(imageId);
         ByteArrayResource resource = new ByteArrayResource(image.getImage().getBytes(1, (int) image.getImage().length()));
