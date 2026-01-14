ae1639fd0d487c310b4d90620809d59b7335982a
Elie <niyubwayoiraelie5777@gmail.com>
2026-01-14 21:43:20 +0200

    Added RequiredArgsConstructor annotation to Controller class for dependency injection.

--- src/main/java/dev/iraelie/e_commerce_backend/controller/CategoryController.java
+++ src/main/java/dev/iraelie/e_commerce_backend/controller/CategoryController.java
@@ -1,7 +1,9 @@
 package dev.iraelie.e_commerce_backend.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class CategoryController {
 }
