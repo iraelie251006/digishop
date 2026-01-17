777ce8e6b7385271649c79a19c12560b5fc4831a
Elie <niyubwayoiraelie5777@gmail.com>
2026-01-17 23:23:47 +0200

    Improved AlreadyExistsException

--- src/main/java/dev/iraelie/e_commerce_backend/exceptions/AlreadyExistsException.java
+++ src/main/java/dev/iraelie/e_commerce_backend/exceptions/AlreadyExistsException.java
@@ -4,4 +4,8 @@ public class AlreadyExistsException extends RuntimeException {
     public AlreadyExistsException(String message) {
         super(message);
     }
+
+    public AlreadyExistsException(String resourceName, String fieldName, Object fieldValue) {
+        super(String.format("%s already exists with %s: %s", resourceName, fieldName, fieldValue));
+    }
 }
