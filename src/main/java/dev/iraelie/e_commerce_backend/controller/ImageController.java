a4052731c0b3afe686399a8d514b44b41071f008
Elie <niyubwayoiraelie5777@gmail.com>
2026-01-18 15:12:16 +0200

    Implemented deleteImage endpoint functionality.

--- src/main/java/dev/iraelie/e_commerce_backend/controller/ImageController.java
+++ src/main/java/dev/iraelie/e_commerce_backend/controller/ImageController.java
@@ -59,4 +59,18 @@ public class ImageController {
         }
         return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("Update failed!", INTERNAL_SERVER_ERROR));
     }
+
+    @DeleteMapping("/image/{imageId}/delete")
+    public ResponseEntity<ApiResponse> deleteImage(@PathVariable Long imageId) {
+        try {
+            Image image = imageService.getImageById(imageId);
+            if(image != null) {
+                imageService.deleteImageById( imageId);
+                return ResponseEntity.ok(new ApiResponse("Delete success!", null));
+            }
+        } catch (ResourceNotFoundException e) {
+            return  ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+        return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("Delete failed!", INTERNAL_SERVER_ERROR));
+    }
 }
